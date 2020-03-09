e650ab3570b72c33b0f576d2e8dde4326a9db48e
Rock-it-science <wilmc17@gmail.com>
2020-03-08 21:24:11 -0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,96 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.PermissionException;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main extends ListenerAdapter {
+    /**
+     * This is the method where the program starts.
+     */
+    public static void main(String[] args) {
+        //We construct a builder for a BOT account. If we wanted to use a CLIENT account
+        // we would use AccountType.CLIENT
+        try {
+            JDA jda = new JDABuilder("Njg2NDAxOTA1MDkzNTA5MjIw.XmW3ww._aOo2xs8cUb_r1d3w7iNkqvTknM")
+                    .addEventListeners(new Main())  // An instance of a class that will handle events.
+                    .build();
+            jda.awaitReady(); // Blocking guarantees that JDA will be completely loaded.
+            System.out.println("Finished Building JDA!");
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        //These are provided with every event in JDA
+        JDA jda = event.getJDA();                       //JDA, the core of the api.
+        long responseNumber = event.getResponseNumber();//The amount of discord events that JDA has received since the last reconnect.
+
+        //Event specific information
+        User author = event.getAuthor();                //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
+        //  This could be a TextChannel, PrivateChannel, or Group!
+
+        String msg = message.getContentDisplay();              //This returns a human readable version of the Message. Similar to
+        // what you would see in the client.
+
+        boolean bot = author.isBot();                    //This boolean is useful to determine if the User that
+        // sent the Message is a BOT or not!
+
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
+        {
+            //Because we now know that this message was sent in a Guild, we can do guild specific things
+            // Note, if you don't check the ChannelType before using these methods, they might return null due
+            // the message possibly not being from a Guild!
+
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (note, in the API, Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
+
+            String name;
+            if (message.isWebhookMessage()) {
+                name = author.getName();                //If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
+            else {
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
+
+            System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
+        } else if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        {
+            //The message was sent in a PrivateChannel.
+            //In this example we don't directly use the privateChannel, however, be sure, there are uses for it!
+            PrivateChannel privateChannel = event.getPrivateChannel();
+
+            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+        }
+
+        if (msg.equals("hi")) {
+            channel.sendMessage("hello there").queue();
+        }
+        else if (msg.equals("give repo")) {
+            channel.sendMessage("").queue();
+        }
+        else if (msg.equals("69")) {
+            channel.sendMessage("nice").queue();
+        }
+        else if (msg.equals("spam")) {
+            for(int i=0; i<25; i++) {
+                channel.sendMessage("hello there").queue();
+            }
+        }
+    }
+}
