f71c9d951097e1062fa03ab628aaa0c85ba758a9
MattB70 <mattrb@telus.net>
2020-03-24 14:43:52 -0700

    Token saving, UI fragements
    I added the ability to save and recall the last used bot token locally. This is ontop of the basic UI structure that is still full of placeholders, but the basic idea works.

--- app/src/main/java/com/example/projectprototype/MainActivity.java
+++ app/src/main/java/com/example/projectprototype/MainActivity.java
@@ -2,43 +2,108 @@ package com.example.projectprototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 import javax.security.auth.login.LoginException;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Views
-        final TextView tv = findViewById(R.id.tv);
-        final Button btn = findViewById(R.id.sendMsg);
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //Send message that was typed in tv
-                try{
-                    JDABuilder.createDefault(TOKEN) //Token goes here
-                            //.addEventListeners(listener) // some other listeners/settings
-                            .addEventListeners(new ListenerAdapter() {
-                                @Override public void onReady(ReadyEvent event) {
-                                    new EventChannel(event.getJDA()).sendGeneral(tv.getText().toString()); // starts your channel with the ready event
-                                }
-                            }).build();
-                } catch(LoginException e){
-                    e.printStackTrace();
+        final TextView tv = findViewById(R.id.editTextToken);
+        tv.setText(getLastToken()); //  Set tv to last used token if available.
+
+        final Button btn = findViewById(R.id.buttonConnect);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if (tv.getText().toString().equals("")       //  Invalid inputs
+                        || tv.getText().toString().length() < 15) {
+                    Toast.makeText(getApplicationContext(), "Invalid Input", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    //  Save token to local file
+                    if (updateTokens(tv.getText().toString()) == 0) {
+                        Toast.makeText(getApplicationContext(), "Token Saved", Toast.LENGTH_SHORT).show();
+                        // Save token to XML (or any other way to save locally)
+                    }
+                    //  Move on to MainNav activity
+                    Intent myIntent = new Intent(MainActivity.this, MainNav.class);
+                    startActivity(myIntent);
+
                 }
             }
         });
+    }
+
+
+
+
+    //  Information on files:
+    //  https://www.dev2qa.com/android-read-write-internal-storage-file-example/
+
+    //  Takes token and returns success states as int
+    int updateTokens(String token) {
+        //  OPs                         :   Return value
+        // save token,                  :   0
+        // FileNotFoundException        :   -1
+        // IOException                  :   -2
+
+        try (FileOutputStream out = new FileOutputStream(new File(getFilesDir(), "token.txt"))) {
+            out.write(token.getBytes());    //  Write token to token.txt
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return -1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -2;
+        }
+        return 0;
+    }
+
+
+
+
+    //  returns saved token from token.txt
+    String getLastToken() {
+        //  OPs                         :   Return value
+        // read token and return it     :   token
+        // return a placeholder         :   ""
+        // error                        :   "error at getLastToken"
+
+        String token;
+        try (BufferedReader br = new BufferedReader(new FileReader(getFilesDir()+"/token.txt"))) {
+            token = br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error at getLastToken";
+        }
+        return token;
     }
 }
---
+++ app/src/main/java/com/example/projectprototype/ui/home/HomeFragment.java
@@ -0,0 +1,94 @@
+package com.example.projectprototype.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.projectprototype.EventChannel;
+import com.example.projectprototype.MainActivity;
+import com.example.projectprototype.R;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
+//  Information on fragments:
+//  https://guides.codepath.com/android/Creating-and-Using-Fragments
+public class HomeFragment extends Fragment {
+    private HomeViewModel homeViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
+
+
+
+    String token;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        //Views
+        final TextView tv = view.findViewById(R.id.tv2);
+        final Button btn = view.findViewById(R.id.sendMsg2);
+        //Token
+        token = getLastToken();
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Send message that was typed in tv
+                try{
+                    JDABuilder.createDefault(token) //Token goes here
+                            //.addEventListeners(listener) // some other listeners/settings
+                            .addEventListeners(new ListenerAdapter() {
+                                @Override public void onReady(ReadyEvent event) {
+                                    new EventChannel(event.getJDA()).sendGeneral(tv.getText().toString()); // starts your channel with the ready event
+                                }
+                            }).build();
+                } catch(LoginException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+
+    //  returns saved token from token.txt
+    String getLastToken() {
+        //  OPs                         :   Return value
+        // read token and return it     :   token
+        // return a placeholder         :   ""
+        // error                        :   "error at getLastToken"
+
+        String token;
+        try (BufferedReader br = new BufferedReader(new FileReader(getContext().getFilesDir()+"/token.txt"))) {
+            token = br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error at getLastToken";
+        }
+        return token;
+    }
+}
