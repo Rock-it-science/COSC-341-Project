9849fd9da24aebba7ebb1e180afeb4fabc442d80
RegisDahma <carter.joel.phillips@gmail.com>
2020-03-26 12:56:11 -0700

    Added Roles Guilds and Server declarations and an addrole function

--- app/src/main/java/com/example/projectprototype/EventChannel.java
+++ app/src/main/java/com/example/projectprototype/EventChannel.java
@@ -2,16 +2,47 @@ package com.example.projectprototype;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+import java.util.List;
+
 
 public class EventChannel {
     private final JDA api;
+    private List users;
+    private List roles;
+    private Guild server;
 
     public EventChannel(JDA api) {
         this.api = api;
+        server = api.getGuilds().get(0);
+        users = server.getMembers();
+        roles = server.getRoles();
     }
+
 
     public void sendGeneral(String msg) {
         TextChannel generalChannel = api.getTextChannelsByName("general", true).get(0);
         generalChannel.sendMessage(msg).queue();
     }
+
+
+    public List getRoles()
+    {
+    	return roles;
+    }
+
+    public List getMembers()
+    {
+    	return users;
+    }
+
+    public void setRole(Member member, Role role)
+    {
+    	server.addRoleToMember(member, role);
+    }
+
+
+
 }
